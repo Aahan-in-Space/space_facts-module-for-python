5ef63a25e4b9502afd3d6ea7582f73fd7b86dc59
2010person <116949561+2010person@users.noreply.github.com>
2022-12-27 11:05:10 +0000

    Updated space_facts.py
    Updated the description at the top and removed a blank line from the bottom.

--- src/space_facts_2010person/space_facts.py
+++ src/space_facts_2010person/space_facts.py
@@ -1,6 +1,6 @@
 # This module is the copyright of Aahan in Space
-# See the LICENSE.md in the GitHub repository (link below) for more details
-# For how-to-use, bugs and more see the README.md on the GitHub repository (link below)
+# See the LICENSE.md here: https://github.com/Aahan-in-Space/space_facts-module-for-python/blob/main/LICENSE.md
+# For how-to-use, bugs and more see the README.md (being worked on).
 # The documentation can be accessed here: https://github.com/Aahan-in-Space/space_facts-module-for-python/wiki
 # If you wish to contribute head over to the GitHub repository https://github.com/aahan-in-space/space_facts-module-for-python
 # This module is not-for-profit and open-source
