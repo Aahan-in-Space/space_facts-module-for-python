d65e40aeb61e36cb927513b7f576d660645ba094
2010person <aahanpathak360@gmail.com>
2022-12-27 11:01:40 +0000

    My initial commit
    This adds a LICENSE.md, the source files and edits the README.md.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # space_facts-module-for-python
 This module teaches your Python code the knowledge of the universe.
+# Contributions will be welcomed soon...
---
+++ src/space_facts_2010person/space_facts.py
@@ -0,0 +1,10 @@
+# This module is the copyright of Aahan in Space
+# See the LICENSE.md in the GitHub repository (link below) for more details
+# For how-to-use, bugs and more see the README.md on the GitHub repository (link below)
+# The documentation can be accessed here: https://github.com/Aahan-in-Space/space_facts-module-for-python/wiki
+# If you wish to contribute head over to the GitHub repository https://github.com/aahan-in-space/space_facts-module-for-python
+# This module is not-for-profit and open-source
+# Hope you enjoy!!!
+
+s_Artemis1_sh = "Artemis 1 was a successful mission that was part of the Artemis Program." \
+                 " It was a successful test of new technologies designed to return humanity to the moon. "
