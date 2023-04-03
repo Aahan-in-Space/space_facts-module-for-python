02d80de7f1cd61437d5f175017f3ff51214d9cf0
2010person <116949561+2010person@users.noreply.github.com>
2023-04-03 13:26:54 +0100

    Update hello.py
    Updated the version

--- src/space_facts/hello.py
+++ src/space_facts/hello.py
@@ -7,6 +7,6 @@ This program is soley for the purposes of providing users version details among 
 def hello():
     print("Hello user welcome to space_facts, this module has been created to bring important astronomical information into your Python code.")
     print("Python is a general-purpose programming language allowing YOU to leverage all of the capabilities provided by this module into whatever you want to make using Python"!)
-    print("Your version of space_facts for Python 3 is v0.001.")
+    print("Your version of space_facts for Python 3 is v1.")
     print("Support and help can be found at the documentation here: https://github.com/Aahan-in-Space/space_facts-module-for-python3/wiki")
     print("Wish to contribute? Thanks! Start here: https://github.com/Aahan-in-Space/space_facts-module-for-python3/blob/main/README.md")
