ae724efeb741ca2514c6b3743260ecfcba0c9b7f
2010person <116949561+2010person@users.noreply.github.com>
2023-04-03 15:10:46 +0100

    Create tests.py
    Created a test program for the continuous integration to test.

---
+++ src/space_facts/tests.py
@@ -0,0 +1,10 @@
+# For CI testing
+# Two import statemnts below
+import space_facts as a
+from space_facts import hello as b
+
+# Asking the program to give a short description on Artemis 1
+print(a.s_Artemis1_sh)
+
+# Asking the program about the module
+b.hello()
